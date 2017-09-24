6a2b1d418849742f96bae48622392e5eb1b249f2
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2017-09-24 04:07:24 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component, } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
-import { Panel, Button, ButtonGroup, ButtonToolbar, FormGroup, FormControl, Grid, Row, Col, } from 'react-bootstrap';
+import { Panel, Button, ButtonGroup, ButtonToolbar, FormGroup, FormControl, InputGroup, Glyphicon, Grid, Row, Col, } from 'react-bootstrap';
 
 const days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun', ];
 
@@ -11,6 +11,8 @@ class App extends Component {
         super(props);
         this.state = {
             cells: Array(10).fill(false).map(() => Array(7).fill(false)),
+            schedules: new Array(0),
+            selection: null,
         };
     }
 
@@ -23,35 +25,34 @@ class App extends Component {
         });
     }
 
-    render() {
-        return (
-            <div className="appWrapper"> 
-                <Grid>
-                    <Row>
-                        <Col lg={6}>
-                            <Table toggle={this.toggle} cells={this.state.cells} />
-                        </Col>
-                        <Col lg={3}>
-                            <Panel style={{textAlign: 'center'}} >
-                                Banner placeholder
-                            </Panel>
-                            <Navigation />
-                        </Col>
-                    </Row>
-                </Grid>
-            </div>
-        );
-    }
-}
-
-class Navigation extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-
     addSchedule = () => {
+        this.setState(() => {
+            const newArr = this.state.schedules.slice();
+            newArr.push('');
+            return { schedules: newArr };
+        });
+    }
 
+    updateSchedule = (i, value) => {
+        this.setState(() => {
+            const newArr = this.state.schedules.slice();
+            newArr[i] = value;
+            return { schedules: newArr };
+        });
+    }
+
+    removeSchedule = (i) => {
+        this.setState(() => {
+            const newArr = this.state.schedules.slice();
+            newArr.splice(i, 1);
+            return { schedules: newArr };
+        })
+    }
+
+    selectSchedule = (i) => {
+        this.setState(() => {
+            return { selection: i };
+        });
     }
 
     report = () => {
@@ -60,12 +61,85 @@ class Navigation extends Component {
 
     render() {
         return (
+            <div className="appWrapper"> 
+                <Grid>
+                    <Row>
+                        <Col lg={6}>
+                            <Table toggle={this.toggle} cells={this.state.cells} />
+                        </Col>
+                        <Navigation 
+                            schedules={this.state.schedules}
+                            selection={this.state.selection}
+                            addSchedule={this.addSchedule}
+                            updateSchedule={this.updateSchedule}
+                            removeSchedule={this.removeSchedule}
+                            selectSchedule={this.selectSchedule}
+                            report={this.report}
+                        />
+                    </Row>
+                </Grid>
+            </div>
+        );
+    }
+}
+
+function Navigation(props){
+    let schedules = [];
+        for (let i = 0; i < props.schedules.length; i++) {
+            schedules.push(<Schedule 
+                                text={props.schedules[i]} 
+                                updateSchedule={props.updateSchedule} 
+                                removeSchedule={props.removeSchedule}
+                                selectSchedule={props.selectSchedule} 
+                                i={i} key={i} 
+                            />)
+    }
+    return(
+        <Col lg={3}>
+            <Panel style={{textAlign: 'center'}} >
+                Banner placeholder
+            </Panel>
             <ButtonToolbar>
                 <ButtonGroup justified>
-                    <Button onClick={this.report}>Report</Button>
-                    <Button onClick={this.addSchedule}>Add Schedule</Button>
+                    <Button onClick={props.report}>Report</Button>
+                    <Button onClick={props.addSchedule}>Add Schedule</Button>
                 </ButtonGroup>
             </ButtonToolbar>
+            <form style={{paddingTop: '5%'}}>
+                {schedules}
+            </form>
+        </Col>
+    );
+}
+
+class Schedule extends Component {
+    handleClick = () => {
+        this.props.removeSchedule(this.props.i);
+    }
+
+    handleChangeText = (e) => {
+        this.props.updateSchedule(this.props.i, e.target.value);
+    }
+
+    handleChangeRadio = (e) => {
+        this.props.selectSchedule(e.target.value);
+    }
+
+    render() {
+        return (
+            <FormGroup>
+                <InputGroup>
+                    <InputGroup.Addon>
+                        <input type="radio" name="radioGroup" value={this.props.i} onChange={this.handleChangeRadio} />
+                    </InputGroup.Addon>
+                    <FormControl onChange={this.handleChangeText} type="text" value={this.props.text} />
+                    <InputGroup.Button>
+                        <Button onClick={this.handleClick} bsStyle="danger">
+                            <Glyphicon glyph="remove" />
+                        </Button>
+                    </InputGroup.Button>
+                </InputGroup>
+            </FormGroup>
         );
     }
 }
@@ -73,9 +147,9 @@ class Navigation extends Component {
 function Table(props) {
     return (
         <table>
-              <Header />
-              <Body toggle={props.toggle} cells={props.cells} />
-            </table>
+            <Header />
+            <Body toggle={props.toggle} cells={props.cells} />
+        </table>
     );
 }
 
